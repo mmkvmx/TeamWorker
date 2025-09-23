1154fc7ec3bb19d50a521ca0c6ceb29588cd9d01
mmkvmx <mmamkov@yandex.ru>
2025-09-23 09:12:00 +0300

    initial commit

---
+++ TeamWork.Domain/Entities/Enums/TaskEnums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork.Domain.Entities.Enums
+{
+    public class TaskEnums
+    {
+        public enum Status
+        {
+            Open,
+            InProgress,
+            Done,
+            Blocked
+        }
+        public enum Priority
+        {
+            Low,
+            Medium,
+            High,
+            Critical
+        }
+    }
+}
---
+++ TeamWork.Domain/Entities/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using TeamWork.Domain.Entities.Enums;
+
+namespace TeamWork.Domain.Entities
+{
+    public class TaskItem : EntityBase
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public TaskEnums.Status Status { get; set; }
+        public TaskEnums.Priority Priority { get; set; }
+        public DateTime DueDate { get; set; }
+        public Guid AssignedToUserId { get; set; }
+    }
+}
