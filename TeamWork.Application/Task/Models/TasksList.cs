b504e97c3d76316ec1c2982c1cf672be056970c0
Maksim Mamkov <madumax1809@gmail.com>
2025-09-28 16:26:31 +0300

    feat: добавлен GetTasksQueryHandler

---
+++ TeamWork.Application/Task/Models/TasksList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamWork.Application.Task.Models
+{
+    public class TasksList
+    {
+        List<TaskItemDto> items { get; set; } = new List<TaskItemDto>();
+    }
+}
--- TeamWork.Application/Task/Queries/GetTasksListQueryHandler.cs
+++ TeamWork.Application/Task/Queries/GetTasksListQueryHandler.cs
@@ -1,13 +1,27 @@
 ﻿using AutoMapper;
 using MediatR;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TeamWork.Application.Interfaces;
+using TeamWork.Application.Task.Models;
 
 namespace TeamWork.Application.Task.Queries
 {
+    public record GetTasksQuery : IRequest<TasksList> { }
 
+    public class GetTasksQueryHandler(ITasksDbContext tasksDbContext, IMapper mapper) 
+    { 
+        public async Task<TasksList> Handle(GetTaskQuery query, CancellationToken cancellationToken)
+        {
+            var tasks = await tasksDbContext.Tasks
+            .ProjectTo<TaskItemDto>(mapper.ConfigurationProvider)
+            .ToListAsync(cancellationToken);
+
+        }
+    }
 }
